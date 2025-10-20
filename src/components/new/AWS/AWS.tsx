4ee3da9ae705c10eb30e670a25ae961f7c852dca
BaharAbbasiDelvand <145874091+BaharAbbasiDelvand@users.noreply.github.com>
2025-10-20 17:50:52 -0400

    created the aws app

--- src/components/new/AWS/AWS.tsx
+++ src/components/new/AWS/AWS.tsx
@@ -1,25 +1,22 @@
-import React from 'react'
-import { AppFrame} from '../app-frames/App-frames';
+import React from "react";
+import "./aws.css";
+import { AppFrame } from "../app-frames/App-frames";
 type AWSProps = { isOpen: boolean; onClose: () => void };
 
-// import "./aws.css";
 export const AWS: React.FC<AWSProps> = ({ isOpen, onClose }) => {
     return (
         <AppFrame
             isOpen={isOpen}
             onClose={onClose}
-            title="My Portfolio"
-            overlay={true}
-
+            title="My AWS Certification"
+            overlay
+            contentClassName="aws-body"
         >
-            <div className="aboutme-main">
-                <h1>AWS Certification</h1>
-                <p>
-                I have earned the AWS Certified Solutions Architect – Associate certification, which validates my expertise in designing and deploying scalable, highly available, and fault-tolerant systems on AWS. This certification demonstrates my ability to effectively utilize AWS services to meet business requirements and optimize cloud solutions.
-                </p>
+            <div className="pdf-wrap">
+                <embed src="/AWSCertification.pdf" type="application/pdf" />
             </div>
         </AppFrame>
-)
-}
+    );
+};
 
-export default AWS
+export default AWS;
