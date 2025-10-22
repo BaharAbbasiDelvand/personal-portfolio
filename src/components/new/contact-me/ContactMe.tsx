ef5bc79419c7a7af1160ee750b3775c5323b8385
BaharAbbasiDelvand <145874091+BaharAbbasiDelvand@users.noreply.github.com>
2025-10-22 17:05:13 -0400

    finished with contact me

--- src/components/new/contact-me/ContactMe.tsx
+++ src/components/new/contact-me/ContactMe.tsx
@@ -6,16 +6,18 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import { LinkedinIcon } from "lucide-react";
+import { LinkedinIcon, Subtitles } from "lucide-react";
+import Button from "@mui/material/Button";
 
 type ContactMeProps = { isOpen: boolean; onClose: () => void };
-
-
 
 export const ContactMe: React.FC<ContactMeProps> = ({ isOpen, onClose }) => {
     const [value, setValue] = React.useState("linkedin");
     const handleChange = (event: React.SyntheticEvent, newValue: string) => {
         setValue(newValue);
+    };
+    const handleEmailButtonClick = () => {
+        window.location.href = "mailto:baharabbasidelvand@gmail.com";
     };
     return (
         <AppFrame
@@ -48,17 +50,58 @@ export const ContactMe: React.FC<ContactMeProps> = ({ isOpen, onClose }) => {
                             />
                             <Tab
                                 value="email"
-                                label="Email"
+                                label="email"
                                 icon={<EmailIcon />}
                             />
                         </Tabs>
                     </Box>
                 </div>
                 <div className="contact-me-content">
-                {value === 'github'   && <Box sx={{p:2}}>GitHub content</Box>}
-{value === 'linkedin' && <Box sx={{p:2}}>LinkedIn content</Box>}
-{value === 'email'    && <Box sx={{p:2}}>Email content</Box>}
-
+                    {value === "github" && (
+                        <Box sx={{ p: 2 }}>
+                            <div className="github-content">
+                                You could stay updated on the coding projects I
+                                do on GitHub!{" "}
+                                <Button
+                                    variant="outlined"
+                                    sx={{ marginTop: 5 }}
+                                    href="https://github.com/BaharAbbasiDelvand"
+                                >
+                                    Go To My GitHub Profile
+                                </Button>
+                            </div>
+                        </Box>
+                    )}
+                    {value === "linkedin" && (
+                        <Box sx={{ p: 2 }}>
+                            <div className="linkedin-content">
+                                You can follow me and/or connect with me on
+                                LinkedIn!{" "}
+                                <Button
+                                    variant="outlined"
+                                    sx={{ marginTop: 5 }}
+                                    href="https://www.linkedin.com/in/baharabbasidelvand/"
+                                >
+                                    Go To My LinkedIn Profile
+                                </Button>
+                            </div>
+                        </Box>
+                    )}
+                    {value === "email" && (
+                        <Box sx={{ p: 2 }}>
+                            <div className="email-content">
+                                Or you can email me! I'm looking forward to
+                                connect!{" "} 
+                                <Button
+                                    variant="outlined"
+                                    sx={{ marginTop: 5 }}
+                                    onClick={handleEmailButtonClick}
+                                >
+                                    Email Me
+                                </Button>
+                            </div>
+                        </Box>
+                    )}
                 </div>
             </div>
         </AppFrame>
